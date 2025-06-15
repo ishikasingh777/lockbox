cfd673feeea924a560da8fd2420577bafb27f8bf
ishikasingh777 <ishisingh1129@gmail.com>
2025-06-15 21:34:31 +0530

    changed buil.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-set -o errexit
+set -o errexit  # Exit on error
 
+# Install dependencies from requirements.txt
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Collect static files (for deployment)
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
